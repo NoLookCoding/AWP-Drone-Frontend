4cae27c3724cb4c02df08bca386e8ad53ae51ec7
SeungGun <saverrw2864@naver.com>
2023-04-08 01:20:45 +0900

    feat: 드론 더미 데이터 추가, 상품 페이지 & 상세 상품 페이지 데이터 연결 및 상태 관리 확장

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import './App.css';
 import React from 'react'
-import {
-  BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Definition from './pages/definition/Layout';
-import Home from './pages/Home';
-import Car from './pages/store/Car';
 import User from './pages/UserInfo';
 import NoPage from './pages/NoPage';
 import Store from './pages/store/Store';
@@ -14,27 +10,21 @@ import Header from './pages/Header';
 import Footer from './pages/Footer';
 import Usage from './pages/usage/Usage'
 
-import { Outlet, Link } from "react-router-dom"
-import Layout from './pages/definition/Layout';
-
-
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element = {<Header/>}>
-        <Route index element = {<Definition/>}/>
-        <Route path="store" element = {<Store/>}/>
-        <Route path="store/:indexOfList" element = {<DetailDrone/>}/>
-        <Route path="userinfo" element = {<User/>}/>
-        <Route path="*" element={<NoPage/>}/>
-        <Route path="/usage" element={<Usage/>}/>
-      </Route>
-    </Routes>
- 
-    <Footer/>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<Definition />} />
+          <Route path="store" element={<Store />} />
+          <Route path="store/:productId" element={<DetailDrone />} />
+          <Route path="userinfo" element={<User />} />
+          <Route path="*" element={<NoPage />} />
+          <Route path="/usage" element={<Usage />} />
+        </Route>
+      </Routes>
+      <Footer />
     </BrowserRouter>
-    
   );
 }
 
