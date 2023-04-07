1b486d048d5488e5e2814d146c1889f47dae3d62
SeungGun <saverrw2864@naver.com>
2023-04-08 01:30:07 +0900

    hotfix: image removed mistakes resolved

--- src/App.js
+++ src/App.js
@@ -11,23 +11,28 @@ import Header from './pages/Header';
 import Footer from './pages/Footer';
 import Usage from './pages/usage/Usage'
 
+const images = ['https://cdn.pixabay.com/photo/2016/11/29/02/59/drone-1866961_1280.jpg',
+  'https://cdn.pixabay.com/photo/2017/09/07/08/57/drone-2724257_1280.jpg',
+  'https://www.cctvnews.co.kr/news/photo/202009/209837_210397_5058.jpg',
+  'https://media.kingston.com/kingston/hero/ktc-hero-blog-personal-storage-drone-photography-tips-lg.jpg'];
+
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Header />}>
-                    <Route index element={<Home images={images} />} />
-                    <Route path="introduce" element={<Introduce />} />
-                    <Route path="store" element={<Store />} />
-                    <Route path="store/:productId" element={<DetailDrone />} />
-                    <Route path="userinfo" element={<User />} />
-                    <Route path="*" element={<NoPage />} />
-                    <Route path="/usage" element={<Usage />} />
-                </Route>
-            </Routes>
-            <Footer />
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<Home images={images} />} />
+          <Route path="introduce" element={<Introduce />} />
+          <Route path="store" element={<Store />} />
+          <Route path="store/:productId" element={<DetailDrone />} />
+          <Route path="userinfo" element={<User />} />
+          <Route path="*" element={<NoPage />} />
+          <Route path="/usage" element={<Usage />} />
+        </Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
