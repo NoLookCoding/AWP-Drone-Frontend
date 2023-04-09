1ed41779dcd5e3a67964e20adb683a76ab15183d
1109min <1109min@naver.com>
2023-04-09 18:47:50 +0900

    Feat: Introduce done

--- src/pages/introduce/IntroduceContent.js
+++ src/pages/introduce/IntroduceContent.js
@@ -16,15 +16,15 @@ const IntroduceContent = (props) => {
   
   const handleScroll = () =>{
     // mini
-    if(window.scrollY >= 850){
+    if(window.scrollY >= 1250){
       setScroll(7);
-    }else if(window.scrollY >750){
+    }else if(window.scrollY >1150){
       setScroll(6);
-    }else if(window.scrollY >=600){
+    }else if(window.scrollY >=750){
       setScroll(5);
-    }else if(window.scrollY >=500){
+    }else if(window.scrollY >=650){
       setScroll(4);
-    }else if(window.scrollY >=200 + 50){
+    }else if(window.scrollY >=250){
       setScroll(3);
     }else if(window.scrollY >=150){
       setScroll(2);
@@ -47,11 +47,11 @@ const IntroduceContent = (props) => {
                     <div className={`introduce-content-mini slide-in ${scroll >= 2 ? 'visible' : ''}`} style={{backgroundImage:`url(${'http://www.ridemag.co.kr/news/photo/202011/15466_94488_3310.jpg'})`}}>                    </div>
                     <div className={`introduce-content-mini-content slide-in ${scroll >= 3 ? 'visible' : ''}`}>
                       <div className="introduce-content-expo-title"> 
-                      2000년에 시작한 <br/>
-                      NOLOOK DRONES는 <br/>
-                      고품질 장비와 <br/>
-                      최신 기술을 바탕으로 <br/> 
-                      최고의 드론 서비스를 제공합니다. <br/> 
+                      2000년에 시작한<br/>
+                      NOLOOK DRONES는<br/>
+                      고품질 장비와<br/>
+                      최신 기술을 바탕으로<br/> 
+                      최고의 드론 서비스를 제공합니다.<br/> 
                       </div>
                     </div>
                   </div>
@@ -60,10 +60,10 @@ const IntroduceContent = (props) => {
                     <div className={`introduce-content-mini-content slide-in ${scroll >= 5 ? 'visible' : ''}`}>
                       <div className="introduce-content-expo-title"> 
                       공연, 촬영, 방역, 시설관리,<br/>
-                      재난, 물류 등 <br/> 
-                      다양한 산업분야에 <br/> 
-                      드론 솔루션을 제공합니다. <br/> 
-                      국방부와도 협업을 진행하고 있습니다. <br/> 
+                      재난, 물류 등<br/> 
+                      다양한 산업분야에<br/> 
+                      드론 솔루션을 제공합니다.<br/> 
+                      국방부와도 협업을 진행하고 있습니다.<br/> 
 
                       </div>
                     </div>
@@ -76,9 +76,10 @@ const IntroduceContent = (props) => {
                     </div>
                     <div className={`introduce-content-mini-content slide-in ${scroll >= 7 ? 'visible' : ''}`}>
                       <div className="introduce-content-expo-title" >
-                        고객의 요구에 따라 <br/> 
-                        맞춤형 서비스를 제공하며, <br/>
-                        최상의 결과를 얻을 수 있도록  <br/> 
+                        고객의 요구에 따라<br/> 
+                        맞춤형 서비스를 제공하며,<br/>
+                        12가지 분야와 협업하여<br/>
+                        최상의 결과를 얻을 수 있도록<br/> 
                         전문가들이 최선을 다하고 있습니다.</div>
                     </div>
                   </div>
